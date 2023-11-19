eaa4c8eb21c3a2c128ddfc3af164aa2a3c1e424c
DodgyDave9335 <151465154+DodgyDave9335@users.noreply.github.com>
2023-11-19 20:14:30 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -28,48 +28,6 @@
 
     nixosConfigurations = { 
  
-      principium = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          username = "eriim";
-          hostname = "principium";
-          displayConfig = "desktop";
-          nvidia_bool = "enabled";
-        } // attrs;        
-        modules = [
-          ./.
-          ./modules/obs
-          ./modules/toys
-          ./modules/virt
-        ];
-      };#principium
-
-      sisyphus = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          username = "eriim";
-          hostname = "sisyphus";
-          displayConfig = "laptop";
-          nvidia_bool = "disabled";
-        } // attrs;        
-        modules = [
-            ./.
-        ];
-      };#sisyphus
-
-      live-image = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          username = "nixos";
-          hostname = "live-image";
-          displayConfig = "laptop";
-          nvidia_bool = "disabled";
-          } // attrs;
-          modules = [
-            ./minimal.nix
-          ];
-      };#live-image
-
     };#configurations
 
     templates.default = {
