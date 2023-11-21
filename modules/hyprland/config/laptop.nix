4e4f2058f3a02c99f8fd5e3b30fb97f7fc936117
DodgyDave9335 <151465154+DodgyDave9335@users.noreply.github.com>
2023-11-21 09:57:41 +0100

    Update laptop.nix

--- modules/hyprland/config/laptop.nix
+++ modules/hyprland/config/laptop.nix
@@ -5,96 +5,57 @@
       ".config/hypr/hyprpaper.conf".text = ''
 preload = ~/.config/backgrounds/232136.png
 preload = ~/.config/backgrounds/1A1C23.png
-wallpaper = eDP-1, ~/.config/backgrounds/232136.png
+wallpaper = DP-1, ~/.config/backgrounds/232136.png
       '';  
 
       ".config/hypr/hyprland.conf".text = ''
-#monitor=edp-1,3840 x 2160,auto,auto
-monitor=,preferred,auto,auto
 
+# monitor
+monitor = DP-1,3840x1600@160,0x0,1
+
+# autostart
 exec-once = hyprpaper & waybar & mako & lxqt-policykit-agent & dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 # some default env vars.
-env=BROWSER, firefox
-env=XCURSOR_SIZE,24
-env=XDG_CURRENT_DESKTOP,Hyprland
-env=XDG_SESSION_DESKTOP,Hyprland
-env=XDG_SESSION_TYPE,wayland
-env=GDK_BACKEND,wayland,x11
-env=QT_QPA_PLATFORM,wayland
+env = XCURSOR_SIZE,24
+env = XDG_CURRENT_DESKTOP,Hyprland
+env = XDG_SESSION_DESKTOP,Hyprland
+env = XDG_SESSION_TYPE,wayland
+env = GDK_BACKEND,wayland,x11
+env = QT_QPA_PLATFORM,wayland
 
-$mainMod = SUPER
-
-bind = $mainMod, q, exec, foot 
-bind = $mainMod, w, killactive, 
-bind = $mainMod, f, fullscreen, 1
-bind = $mainMod, d, exec, pavucontrol
-bind = $mainMod, m, exit, 
-bind = $mainMod, e, exec, kitty -e ranger
-bind = $mainMod, b, exec, foot -e btop
-bind = $mainMod, v, togglefloating, 
-bind = $mainMod, r, exec, wofi --show drun
-bind = $mainMod, p, pseudo, # dwindle
-bind = $mainMod, j, togglesplit, # dwindle
-bind = $mainMod, 0, exec, swaylock
-bind = $mainMod, s, exec, grim -g "$(slurp)" ~/Pictures/Screenshots/$(date +'%Y%m%d_%H%M%S').png
-
-$w1 = hyprctl hyprpaper wallpaper "eDP-1, ~/.config/backgrounds/232136.png"
-$w2 = hyprctl hyprpaper wallpaper "eDP-1, ~/.config/backgrounds/1A1C23.png"
-$w3 = hyprctl hyprpaper wallpaper "eDP-1, ~/.config/backgrounds/232136.png"
-$w4 = hyprctl hyprpaper wallpaper "eDP-1, ~/.config/backgrounds/232136.png"
-$w5 = hyprctl hyprpaper wallpaper "eDP-1, ~/.config/backgrounds/232136.png"
-$w6 = hyprctl hyprpaper wallpaper "eDP-1, ~/.config/backgrounds/232136.png"
-$w7 = hyprctl hyprpaper wallpaper "eDP-1, ~/.config/backgrounds/232136.png"
-
-$wA = Alpha
-$wB = Bravo
-$wC = Charlie
-$wD = Delta
-$wE = Echo
-$wF = Foxtrot
-$wG = Golf
-$wH = Hotel
-$wI = are ya lost bud
-
-# for all categories, see https://wiki.hyprland.org/configuring/variables/
+# general
 input {
-	kb_layout = us
-	follow_mouse = 1
-	touchpad {
-		natural_scroll = no
-	}
-	sensitivity = -0.1 # -1.0 - 1.0, 0 means no modification.
+	kb_layout = uk
 }
 
 general {
-	gaps_in = 5
-	gaps_out = 10
-	border_size = 2
-	col.active_border = rgba(0D599Fee) rgba(ffffffee) 45deg
-	col.inactive_border = rgba(595959aa)
-	layout = dwindle
-	cursor_inactive_timeout = 15
-	no_cursor_warps = yes
-	resize_on_border = yes
-	hover_icon_on_border = yes
+	gaps_in			= 5
+	gaps_out		= 10
+	border_size		= 2
+	col.active_border	= rgba(0D599Fee) rgba(ffffffee) 45deg
+	col.inactive_border	= rgba(595959aa)
+	layout			= dwindle
+	no_cursor_warps		= yes
+	resize_on_border	= yes
+	hover_icon_on_border	= yes
 }
 
 decoration {
-	rounding = 10
-	drop_shadow = yes
-	shadow_range = 4
-	shadow_render_power = 3
-	col.shadow = rgba(1a1a1aee)
-	active_opacity = 1.0
-	inactive_opacity = 1.0
-	fullscreen_opacity = 1.0
+	rounding		= 10
+	drop_shadow		= yes
+	shadow_range		= 4
+	shadow_render_power	= 3
+	col.shadow		= rgba(1a1a1aee)
+	active_opacity		= 0.84
+	inactive_opacity	= 0.84
+	fullscreen_opacity	= 0.84
 	blur {
-		enabled = yes
-		size = 8
-		passes = 3
+		enabled		  = yes
+		size		  = 8
+		passes		  = 3
 		new_optimizations = true
-		xray = true
+		xray		  = true
 		}
 }
 
@@ -111,8 +72,8 @@ animations {
 }
 
 dwindle {
-	pseudotile = yes # master switch for pseudotiling. enabling is bound to mainmod + p in the keybinds section below
-	preserve_split = yes # you probably want this
+	pseudotile = yes
+	preserve_split = yes
 }
 
 master {
@@ -128,86 +89,61 @@ device:epic-mouse-v1 {
 }
 
 misc {
-	disable_hyprland_logo = true
-	enable_swallow = true
-	swallow_regex = ^(foot)$
-	background_color = 0x232136
+	disable_hyprland_logo	 = true
+  	disable_splash_rendering =true
+	mouse_move_enables_dpms	 =true
+	background_color	 = 0x232136
 }
 
-bind = $mainMod, left, movefocus, l
+#keybinds
+$mainMod = SUPER
+
+# Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+bind = $mainMod SHIFT, 	Q, 		killactive, 
+bind = $mainMod SHIFT, 	E, 		exit,
+bind = $mainMod SHIFT, 	F, 		togglefloating, 
+bind = $mainMod SHIFT, 	P, 		pseudo, 	# dwindle
+bind = $mainMod SHIFT, 	J, 		togglesplit, 	# dwindle
+bind = $mainMod SHIFT,	Print,	exec,	grim -g "$(slurp)"
+bind = $mainMod,	Print,	exec,	grim
+bind = $mainMod,	Return,	exec,	alacritty
+bind = $mainMod,	Z,	exec,	~/.config/rofi/launchers/type-1/launcher.sh
+
+# Move focus with mainMod + arrow keys
+bind = $mainMod, left, 	movefocus, l
 bind = $mainMod, right, movefocus, r
-bind = $mainMod, up, movefocus, u
-bind = $mainMod, down, movefocus, d
+bind = $mainMod, up, 	movefocus, u
+bind = $mainMod, down, 	movefocus, d
 
-bind = $mainMod shift, left, movewindow, l
-bind = $mainMod shift, right, movewindow, r
-bind = $mainMod shift, up, movewindow, u
-bind = $mainMod shift, down, movewindow, d
+# Switch workspaces with mainMod + [0-9]
+bind = $mainMod, 1, workspace, 1
+bind = $mainMod, 2, workspace, 2
+bind = $mainMod, 3, workspace, 3
+bind = $mainMod, 4, workspace, 4
+bind = $mainMod, 5, workspace, 5
+bind = $mainMod, 6, workspace, 6
+bind = $mainMod, 7, workspace, 7
+bind = $mainMod, 8, workspace, 8
+bind = $mainMod, 9, workspace, 9
+bind = $mainMod, 0, workspace, 10
 
+# Move active window to a workspace with mainMod + SHIFT + [0-9]
+bind = $mainMod SHIFT, 1, movetoworkspacesilent, 1
+bind = $mainMod SHIFT, 2, movetoworkspacesilent, 2
+bind = $mainMod SHIFT, 3, movetoworkspacesilent, 3
+bind = $mainMod SHIFT, 4, movetoworkspacesilent, 4
+bind = $mainMod SHIFT, 5, movetoworkspacesilent, 5
+bind = $mainMod SHIFT, 6, movetoworkspacesilent, 6
+bind = $mainMod SHIFT, 7, movetoworkspacesilent, 7
+bind = $mainMod SHIFT, 8, movetoworkspacesilent, 8
+bind = $mainMod SHIFT, 9, movetoworkspacesilent, 9
+bind = $mainMod SHIFT, 0, movetoworkspacesilent, 10
 
-workspace=name:$wA,monitor:eDP-1,default:true
-workspace=name:$wB,monitor:eDP-1
-workspace=name:$wC,monitor:eDP-1
-workspace=name:$wD,monitor:eDP-1
-workspace=name:$wE,monitor:eDP-1
-workspace=name:$wF,monitor:eDP-1
-workspace=name:$wG,monitor:eDP-1
-workspace=name:$wH,monitor:eDP-1
-workspace=name:$wI,monitor:eDP-1
+# Scroll through existing workspaces with mainMod + scroll
+bind = $mainMod, mouse_up, 	workspace, e+1
+bind = $mainMod, mouse_down, 	workspace, e-1
 
-bind = $mainMod, 1, workspace, name:$wA
-bind = $mainMod, 1, exec,$w1
-
-bind = $mainMod, 2, workspace, name:$wB
-bind = $mainMod, 2, exec,$w2
-
-bind = $mainMod, 3, workspace, name:$wC
-bind = $mainMod, 3, exec,$w3
-
-bind = $mainMod, 4, workspace, name:$wD
-bind = $mainMod, 4, exec,$w4
-
-bind = $mainMod, 5, workspace, name:$wE
-bind = $mainMod, 5, exec,$w5
-
-bind = $mainMod, 6, workspace, name:$wF
-bind = $mainMod, 6, exec,$w6
-
-bind = $mainMod, 7, workspace, name:$wG
-bind = $mainMod, 7, exec,$w7
-
-bind = $mainMod, 8, workspace, name:$wH
-
-bind = $mainMod, 9, workspace, name:$wI
-
-bind = $mainMod SHIFT, 1, movetoworkspace,name:$wA
-bind = $mainMod SHIFT, 1, exec,$w1
-
-bind = $mainMod SHIFT, 2, movetoworkspace,name:$wB
-bind = $mainMod SHIFT, 2, exec,$w2
-
-bind = $mainMod SHIFT, 3, movetoworkspace,name:$wC
-bind = $mainMod SHIFT, 3, exec,$w3
-
-bind = $mainMod SHIFT, 4, movetoworkspace,name:$wD
-bind = $mainMod SHIFT, 4, exec,$w4
-
-bind = $mainMod SHIFT, 5, movetoworkspace,name:$wE
-bind = $mainMod SHIFT, 5, exec,$w5
-
-bind = $mainMod SHIFT, 6, movetoworkspace,name:$wF
-bind = $mainMod SHIFT, 6, exec,$w6
-
-bind = $mainMod SHIFT, 7, movetoworkspace,name:$wG
-bind = $mainMod SHIFT, 7, exec,$w7
-
-bind = $mainMod SHIFT, 8, movetoworkspace,name:$wH
-
-bind = $mainMod SHIFT, 9, movetoworkspace, name:$wI
-
-bind = $mainMod, mouse_down, workspace, e+1
-bind = $mainMod, mouse_up, workspace, e-1
-
+# Move/resize windows with mainMod + LMB/RMB and dragging
 bindm = $mainMod, mouse:272, movewindow
 bindm = $mainMod, mouse:273, resizewindow
       '';
