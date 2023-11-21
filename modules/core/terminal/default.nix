cf6c1769650a0a7795175da845e71f59986291f2
DodgyDave9335 <151465154+DodgyDave9335@users.noreply.github.com>
2023-11-21 14:23:55 +0100

    Update default.nix

--- modules/core/terminal/default.nix
+++ modules/core/terminal/default.nix
@@ -12,13 +12,11 @@
    # ---- Home Configuration ----
    home-manager.users.${username} = { pkgs, ... }: {
      programs.git.enable = true;
-     programs.vim.enable = true;
    };
 
    # ---- System Configuration ----
 
    environment.systemPackages = with pkgs; [
-     alacritty
      btop
      nitch
      neofetch
