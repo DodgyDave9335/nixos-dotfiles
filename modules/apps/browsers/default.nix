c14bc6732403baf7ecc271e65b6bba0573cc5bfd
DodgyDave9335 <151465154+DodgyDave9335@users.noreply.github.com>
2023-11-21 18:29:09 +0000

    Update default.nix

--- modules/apps/browsers/default.nix
+++ modules/apps/browsers/default.nix
@@ -4,37 +4,13 @@
   home-manager.users.${username} = { pkgs, ... }: {
   
     home.packages = with pkgs; [
-      nyxt
-      qutebrowser
+      librewolf
     ];
 
     home.sessionVariables = {
       MOZ_ENABLE_WAYLAND = 1;
     };
-    
-    programs = {
-      chromium = {
-    	enable = true;
-      };
-      firefox.enable = true;
-    };
   };
 
   #----- System Configuration -----
-  programs = {
-    chromium = {
-	enable = true;
-	extraOpts = {
-	  "BrowserSignin" = 1;
-	  "SyncDisabled" = false;
-	  "PasswordManagerEnabled" = false;
-	  "SpellcheckEnabled" = true;
-	  "SpellcheckLanguage" = [ "en-US" ];
-	  };
-	extensions = [
-	  "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
-	  #"aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1password
-      ];
-    };
-  };
 }
