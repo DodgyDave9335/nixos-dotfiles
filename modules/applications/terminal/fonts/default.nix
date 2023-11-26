9bcac2d2374c643ae0e4b38c7dd80aa04eb63613
DodgyDave9335 <151465154+DodgyDave9335@users.noreply.github.com>
2023-11-26 10:55:50 +0000

    Update default.nix

--- modules/applications/terminal/fonts/default.nix
+++ modules/applications/terminal/fonts/default.nix
@@ -2,17 +2,7 @@
 {  
   fonts = {
     packages = with pkgs; [ 
-	(callPackage (import ./MplusCodeNerdFont) {})
 	nerdfonts
-	noto-fonts
-	noto-fonts-cjk
-	noto-fonts-emoji
-	roboto
 	];
-	fontconfig.defaultFonts = {
-		serif = ["Roboto Serif" "Noto Color Emoji"];
-		sansSerif = [ "Roboto" "Noto Color Emoji" ];
-		emoji = [ "Noto Color Emoji" ];
-	};
    };
 }
