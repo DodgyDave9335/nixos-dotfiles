6dd8a2f3cbf579db6a15d23109b5f18c1a0a8e46
DodgyDave9335 <151465154+DodgyDave9335@users.noreply.github.com>
2024-03-03 17:14:48 +0000

    Update desktop.nix

--- modules/hyprland/config/desktop.nix
+++ modules/hyprland/config/desktop.nix
@@ -9,7 +9,7 @@ home-manager.users.${username} =  { ... }: {
 monitor = DP-1,3840x1600@160,0x0,1
 
 # autostart
-exec-once = swww init && swww img ~/.config/backgrounds/nix-wallpaper-dracula.jpg 
+exec-once = swww init && swww img ~/.config/backgrounds/wall1.jpg 
 exec-once = waybar
 exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
